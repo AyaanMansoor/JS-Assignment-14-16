2233af47e31e51a93a0cf8b8c394ed15cee3c03a
AyaanMansoor <154600399+AyaanMansoor@users.noreply.github.com>
2024-05-02 18:10:35 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,85 @@
+//Q1
+// let futureStudentNames = [];
+
+//q3
+//let fruits = ["apple", "banana", "orange", "grape"];
+
+
+//q4
+//let num = [1,2,3,4,5]
+//q5
+// let booleanArray = [true, false, true, false];
+
+//q6
+//let mixedArray = [10, "apple", true, null];
+
+//q7
+// let edu = ["SSC", "HSC", "BCS","BS", "BCOM", "MS", "MPhil", "PhD" ]
+// document.write( edu.join("<br>") )
+
+//q8
+
+
+// let stuedentName = ["Michael","John","Tony"]
+// let scores = [320,230,480]
+// let totalMarks = 500;
+
+
+// document.write(`Score of ${stuedentName[0]} is ${scores[0]}, Percentage : ${(scores[0]/ totalMarks)*100}% <br>`)
+// document.write(`Score of ${stuedentName[1]} is ${scores[1]}, Percentage : ${(scores[1]/totalMarks)*100}% <br>`)
+// document.write(`Score of ${stuedentName[2]} is ${scores[2]}, Percentage : ${(scores[2]/totalMarks)*100}% <br>`)
+
+// -------------------------------------------------------------------------------------------------------------------------------------//
+
+//q9
+// let colors = ["red","green","blue"]
+// document.write(`<b>Original Array</b> : ${colors} <br>`)
+
+// let colorToAddBeginning = prompt("Enter a color to add to the beginning")
+// colors.unshift(colorToAddBeginning)
+// document.write(`<b>After adding color to the beginning:</b> ${colors} <br>`)
+
+// let colorToAddEnd = prompt("Enter any color to addd to the end")
+// colors.push(colorToAddEnd)
+// document.write(`<b>After adding color to the end:</b> ${colors}<br>`)
+
+
+// colors.unshift("yellow", "orange")
+// document.write(`<b>After adding two colors to the beginning:</b> ${colors}<br>`);
+
+
+// colors.pop()
+// document.write(`<b>After deleting the last color</b>: ${colors}<br>`)
+
+
+// let indexToAdd = prompt("Enter the index where you want to add the color")
+// let colorToAdd = prompt("Enter the color you want to add")
+// colors.splice(indexToAdd,0,colorToAdd)
+// document.write(`<b>After adding colors at index:</b> ${colors}<br>`)
+
+
+// let indexToRemove = prompt("Enter the index from which u have to remove colors")
+// let colorsToRemove = prompt("Enter the number of colors you want to remove")
+// colors.splice(indexToRemove,colorsToRemove)
+// document.write(`<b>After removing colors at index:</b> ${colors}<br>`);
+
+
+
+//x------------------------------------------------------------------x//
+
+// let cities = ["Karachi", "Lahore", "Isl", "Peshawar", "Quetta"];
+
+// let selectedCities = cities.slice(0, 3);
+
+// console.log("Selected Cities:", selectedCities);
+//x------------------------------------------------------------------x//
+
+
+// let arr = ["This","is","my","cat"]
+// let combinedString = arr.join(" ")
+// console.log(combinedString)
+
+
+//x------------------------------------------------------------------x//
+
+
